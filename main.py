1bc2dbaaa2aa6cc45ebd15ae44408dcad79cd56f
rithwikhvarma <rithwikh.v@gmail.com>
2022-05-11 12:54:51 -0700

    Ⓐⓓⓓⓘⓣⓘⓞⓝ ⓞⓕ ⓣⓗⓔ Ⓞⓥⓔⓡⓥⓘⓔⓦ ⓟⓐⓖⓔ ⓣⓞ ⓣⓗⓔ ⓦⓔⓑⓢⓘⓣⓔ

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ def Lucas():
 def Jackson():
     return render_template("jachomepage.html")
 
-@app.route('/Dune/')
-def Dune():
-    return render_template("Dune.html")
+@app.route('/overview/')
+def overview():
+    return render_template("overview.html")
 
 @app.route('/Test/')
 def Test():
